a27b5c6fdafaeeaca22be48803d530584f8395d4
abolfazl <abolfazl>
2023-06-02 17:31:45 +0330

    s

---
+++ hooks/useOnScreen.jsx
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function useOnScreen(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) =>
+        setIntersecting(entry.isIntersecting)
+      ),
+    [ref]
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return isIntersecting;
+}
